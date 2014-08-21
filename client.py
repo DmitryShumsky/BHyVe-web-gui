dc1b8f5b01548e7208ef81ceca9c669c1b7d843c
DmitryShumsky <shumsky90@gmail.com>
2014-08-21 20:38:20 +0300

    Update client.py

--- client.py
+++ client.py
@@ -25,15 +25,14 @@ def decrypt():
     publicKey = connectToServer('localhost',9001,"hello;"+clientID)
     print publicKey+'   - public key'
     firstPrivateKey = connectToServer('localhost',9001,clientID+publicKey)
-    print firstPrivateKey + '         received first private key'
+    #print firstPrivateKey
     privateKey = AES.new(publicKey, AES.MODE_CBC, IV).decrypt(firstPrivateKey)
-    print privateKey+'     first private key'
-    serverAnswer = connectToServer('localhost',9001,clientID+firstPrivateKey)
-    secondPrivateKey = AES.new(privateKey, AES.MODE_CBC, IV).decrypt(serverAnswer)
-    print secondPrivateKey + '           second private key'
-    command = AES.new(secondPrivateKey, AES.MODE_CBC, IV).encrypt('first encrypted command')
-    serverAnswer = connectToServer('localhost',9001,clientID+command)
-    print serverAnswer
+    print privateKey + '         firstPrivateKey'
+    secondPrivateKey = connectToServer('localhost',9001,clientID+firstPrivateKey)
+    print(secondPrivateKey)
+    secondPrivateKeyDecr = AES.new(privateKey, AES.MODE_CBC, IV).decrypt(secondPrivateKey)
+    print secondPrivateKeyDecr+'        - second private key'
+
     #if auth == 0:
     #receivedData =
         #return configArray;
