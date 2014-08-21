2bfc27bf60a2f6f5a845562b61d79993bf7eb086
DmitryShumsky <shumsky90@gmail.com>
2014-08-21 16:43:42 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,16 @@
 __author__ = 'DmitryShumsky'
 #This is server side script. Wait for connect on specified port
+import string
+from os import urandom
+from Crypto.Cipher import AES
+import select
+import socket
+global auth
+import hashlib
+import random
+auth = {}
+IV = 16 * '\x00'
+char_set = string.ascii_uppercase + string.digits
 def parseConfig(configFile):
 #Parse main config file
     import csv
@@ -20,8 +31,7 @@ def parseConfig(configFile):
     return configArray;
 def listenPort(portNumber, listenIP):
 #main function: listen specified port and handle requests
-    import select
-    import socket
+
     portNumber=int(portNumber)
     global server_socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -39,28 +49,51 @@ def listenPort(portNumber, listenIP):
                 read_list.append(client_socket)
                 #print "Connection from", address
             else:
-                data = s.recv(1024)
+                data = s.recv(2048)
                 if data:
                     dataWithoutSymbols = data.strip()
                     dataToWrite = encryption(dataWithoutSymbols)
-                    client_socket.send(dataToWrite)
-                else:
-                    s.close()
-                    read_list.remove(s)
-def encryption(receivedMessage,clientIP):
+                    client_socket.send(str(dataToWrite))
+                #else:
+                    #s.close()
+                    #read_list.remove(s)
+def encryption(receivedMessage):
 #in this function we generate, encrypt and decrypt all messages
-    from os import urandom
-    from Crypto.Cipher import AES
-    auth = {}
-        if receivedMessage == 'hello':
-            auth[clientIP] = 1
-            return urandom(32)
-        else:
-            if auth[clientIP] == 1:
-                
-
-
-
+    if receivedMessage == 'hello':
+        clientID = hashlib.md5(urandom(120)).hexdigest().upper()
+        try:
+            auth[clientID]
+        except KeyError:
+            auth[clientID] = {'encryptionStage': 0, 'key':'','id':clientID}
+            print 'client ID='+clientID
+            return "hello;"+clientID
+    elif receivedMessage[:5] == 'hello':
+        clientID = receivedMessage[6:]
+        #generate puplic key
+        auth[clientID]['key'] = hashlib.md5(''.join(random.sample(char_set*6, 15))).hexdigest().upper()
+        auth[clientID]['encryptionStage'] = 1
+        print auth[clientID]['key'] + "          public key"
+        return auth[clientID]['key']
+    if auth[receivedMessage[:32]]['encryptionStage'] == 1:
+        if auth[receivedMessage[:32]]['key'] == receivedMessage[32:]:
+            #generate first private key
+            auth[receivedMessage[:32]]['key'] = hashlib.md5(''.join(random.sample(char_set*6, 15))).hexdigest().upper()
+            encryptedFirstPrivateKey = AES.new(receivedMessage[32:], AES.MODE_CBC, IV).encrypt(hashlib.md5(''.join(random.sample(char_set*6, 15))).hexdigest().upper())
+            print auth[receivedMessage[:32]]['key'] + "           firstPrivateKey"
+            auth[receivedMessage[:32]]['encryptionStage'] = 2
+            auth[receivedMessage[:32]]['key'] = encryptedFirstPrivateKey
+            return encryptedFirstPrivateKey
+    if auth[receivedMessage[:32]]['encryptionStage'] == 2:
+        if auth[receivedMessage[:32]]['key'] == receivedMessage[32:]:
+            #generate second private key
+            encryptedSecondPrivateKey = AES.new(receivedMessage[32:], AES.MODE_CBC, IV).encrypt(hashlib.md5(''.join(random.sample(char_set*6, 15))).hexdigest().upper())
+            auth[receivedMessage[:32]]['key'] = hashlib.md5(''.join(random.sample(char_set*6, 15))).hexdigest().upper()
+            print auth[receivedMessage[:32]]['key'] + "           second Private Key"
+            auth[receivedMessage[:32]]['key'] = encryptedSecondPrivateKey
+            auth[receivedMessage[:32]]['encryptionStage'] = 3
+            return auth[receivedMessage[:32]]['key']
+    if auth[receivedMessage[:32]]['encryptionStage'] == 3:
+        print AES.new(auth[receivedMessage[:32]]['key'], AES.MODE_CBC, IV).decrypt(receivedMessage[32:])
 
 
 
@@ -68,5 +101,5 @@ def encryption(receivedMessage,clientIP):
 
 
 configArray=parseConfig('/usr/local/etc/bhyvemc/bhyvemc.conf')
-print encryption('asdasdasd')
+print encryption('hello')
 listenPort(configArray["listenPort"],configArray["listenIP"])
